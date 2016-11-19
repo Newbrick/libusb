cc2de0289c82ba6dfffb057fd5d0811e8d77c787
Julio Tentor <jtentor@gmail.com>
2016-11-19 12:52:19 -0300

    Compile libusb*.so for android

---
+++ android/obj/local/mips/objs/usb1.0/__/__/libusb/os/threads_posix.o.d
@@ -0,0 +1,23 @@
+C:/Users/jtentor/Android/libusb/android/obj/local/mips/objs/usb1.0/__/__/libusb/os/threads_posix.o: \
+  C:/Users/jtentor/Android/libusb/android/jni/../../libusb/os/threads_posix.c \
+  C:/Users/jtentor/Android/libusb/android/jni/..\config.h \
+  C:/Users/jtentor/Android/libusb/android/jni/../../libusb/os/threads_posix.h \
+  C:/Users/jtentor/Android/libusb/android/jni/../../libusb\libusbi.h \
+  C:/Users/jtentor/Android/libusb/android/jni/../../libusb/libusb.h \
+  C:/Users/jtentor/Android/libusb/android/jni/../../libusb/version.h \
+  C:/Users/jtentor/Android/libusb/android/jni/../../libusb/version_nano.h \
+  C:/Users/jtentor/Android/libusb/android/jni/../../libusb/os/poll_posix.h
+
+C:/Users/jtentor/Android/libusb/android/jni/..\config.h:
+
+C:/Users/jtentor/Android/libusb/android/jni/../../libusb/os/threads_posix.h:
+
+C:/Users/jtentor/Android/libusb/android/jni/../../libusb\libusbi.h:
+
+C:/Users/jtentor/Android/libusb/android/jni/../../libusb/libusb.h:
+
+C:/Users/jtentor/Android/libusb/android/jni/../../libusb/version.h:
+
+C:/Users/jtentor/Android/libusb/android/jni/../../libusb/version_nano.h:
+
+C:/Users/jtentor/Android/libusb/android/jni/../../libusb/os/poll_posix.h:
